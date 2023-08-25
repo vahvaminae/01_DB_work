06ee1e5fe8000749413b90f7c22865c92b7aec88
vahvaminae <JenifferLeutert@gmx.de>
2023-08-25 10:53:42 +0000

    Show Databases if exist

---
+++ 02_SQL_Tables/02a_db_table_SQL.sql
@@ -0,0 +1,16 @@
+/* ------  Strukturen ----- */
+
+/* Kommentar 1 */
+#  Kommentar 2
+-- Kommentar 3
+
+/* Datenbanken auf Server anzeigen */
+SHOW DATABASES;
+
+#CREATE DATABASE IF NOT EXISTS boo;
+
+#SHOW DATABASES;
+
+DROP DATABASE IF EXISTS boo;
+
+SHOW DATABASES;
--- 2_SQL_Tables/02a_db_table_SQL.sql
+++
@@ -1,9 +0,0 @@
-/* ------  Strukturen ----- */
-
-/* Kommentar 1 */
-#  Kommentar 2
--- Kommentar 3
-
-/* Datenbanken auf Server anzeigen */
-SHOW DATABASES;
-
