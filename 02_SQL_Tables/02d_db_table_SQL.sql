27b43b1628ab4779502e195f93040dc7a5e8c5fb
vahvaminae <JenifferLeutert@gmx.de>
2023-08-28 10:17:18 +0000

    AUTO INCREMENT

---
+++ 02_SQL_Tables/02d_db_table_SQL.sql
@@ -0,0 +1,41 @@
+/* Tabelle mit ID als Primary KEY ---> Keine Duplikate möglich
+AUTO_INCREMENT ++
+DEFAULT Werte eintragen */
+
+/* tabelle löschen falls vorhanden*/
+
+DROP TABLE IF EXISTS boo.cats; 
+
+
+/*Tabelle cats anlegen, falls noch nicht vorhanden */
+
+CREATE TABLE IF NOT EXISTS boo.cats
+(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(20) NOT NULL DEFAULT "TBA",
+    age INT NOT NULL DEFAULT 0
+
+);
+
+
+/*Struktur der Tabelle anzeigen*/
+
+DESCRIBE boo.cats;
+
+/* ----- Daten---- */
+
+/* ID & Default immer zusammen schreiben*/
+
+INSERT INTO boo.cats(id,name,age) VALUES (DEFAULT, "Grizabella", 29);
+INSERT INTO boo.cats(id,age,name) VALUES (DEFAULT, 35, "Alonzo");
+INSERT INTO boo.cats VALUES ();
+
+/* Achtung nur werden DOPPELTE Datensätze zugelassen*/
+
+INSERT INTO boo.cats(age,name) VALUES (35, "Alonzo");
+INSERT INTO boo.cats(age,name) VALUES (35, "Alonzo");
+
+/* Inhalte der Tabelle anzeiten*/ 
+
+SELECT * FROM boo.cats; 
+
