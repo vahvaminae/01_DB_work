23343887da74aa9aaf85ea39744d3c7e013af36f
vahvaminae <JenifferLeutert@gmx.de>
2023-08-28 08:47:12 +0000

    NULL verändern durch Defaultwerte

---
+++ 02_SQL_Tables/02b_db_table_SQL.sql
@@ -0,0 +1,55 @@
+/*Strukturen */
+
+/* NULL wird nicht mehr zugelassen
+DEFAULT Werte eintragen */
+
+/* Datenbanken auf Server anzeigen */
+
+SHOW DATABASES;
+
+/* DB boo löschen, falls vorhanden */
+
+DROP DATABASE IF EXISTS boo;
+
+
+/*DB boo anlegen, falls noch nicht vorhanden*/
+
+CREATE DATABASE IF NOT EXISTS boo;
+
+/* DB auswählen*/
+
+USE boo;
+
+/*Tabelle cats anlegen, falls noch nicht vorhanden */
+
+CREATE TABLE IF NOT EXISTS cats
+(
+
+    name VARCHAR(20) NOT NULL DEFAULT "TBA",
+    age INT NOT NULL DEFAULT 0
+
+);
+
+/*Alle Tabellen in DB anzeigen*/
+
+SHOW TABLES;
+
+/*Struktur der Tabelle anzeigen*/
+
+DESCRIBE cats;
+
+/* ----- Daten---- */
+
+INSERT INTO cats(name,age) VALUES ("Grizabella", 29);
+INSERT INTO cats(age,name) VALUES (35, "Alonzo");
+INSERT INTO cats VALUES ();
+
+/* Achtung nur werden DOPPELTE Datensätze zugelassen*/
+
+INSERT INTO cats(age,name) VALUES (35, "Alonzo");
+INSERT INTO cats(age,name) VALUES (35, "Alonzo");
+
+/* Inhalte der Tabelle anzeiten*/ 
+
+SELECT * FROM cats; 
+
